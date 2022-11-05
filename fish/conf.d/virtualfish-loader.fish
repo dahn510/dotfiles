e93fc36f7d545c834d0face650de3025f2f87de3
Daniel Ahn <dahn510@gmail.com>
2022-11-04 17:53:19 -0700

    .

--- fish/conf.d/virtualfish-loader.fish
+++
@@ -1,5 +0,0 @@
-set -g VIRTUALFISH_VERSION 2.5.4
-set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python
-source /home/danny/.local/lib/python3.10/site-packages/virtualfish/virtual.fish
-source /home/danny/.local/lib/python3.10/site-packages/virtualfish/auto_activation.fish
-emit virtualfish_did_setup_plugins
